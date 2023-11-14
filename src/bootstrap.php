c759d255b1d05d8090bb094ee34d012e21c21a15
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-14 16:40:50 +0300

    fix: rewrite the composer json to match project

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,4 +7,9 @@ require "functions.php";
 
 $config = Config::load();
 
+
+//change TimeZone
+date_default_timezone_set($config['app'][timezone]); 
+
+//set up the database connection
 $database = new QueryBuilder(Connection::make($config['db'])));
