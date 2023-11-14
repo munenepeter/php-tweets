5c8bc3a5b7d83644d075f79c76f7dd78138cf932
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-14 18:08:14 +0300

    better index structure - done with the refactor

--- index.php
+++ index.php
@@ -8,5 +8,18 @@ $url = $_GET['url'];
 
 //get username from url
 $username = getTwitterUsername($url);
-$tweets = getUserTweets($username);
 
+$tweets = Tweet::getTweets();
+
+echo "Viewing {$username}'s tweets as seen from X";
+
+echo json_encode($tweets);
+
+echo "saving {$username}'s tweets to the db and downloading associated media";
+
+for ($i=0; $i < count($tweets) ; $i++) { 
+   echo "saving {$i} of " . count($tweets);
+   Tweet::saveTweet($tweets[$i]);
+}
+
+echo "Done saving all of {$username}'s " . count($tweets) . " tweets";
